2a009318112e494a0847f14a85ef8eb76ccf5268
Sushree Nadiminty <sushreenadiminty@Sushrees-MacBook-Air-2.local>
2025-05-06 12:33:38 -0400

    added backend route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,7 +13,7 @@ const App: React.FC = () => {
     setLoading(true);
     setError('');
     try {
-      const response = await fetch('http://127.0.0.1:5000/predict', {
+      const response = await fetch('https://drugdiscoveryusingdl.onrender.com/predict', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ smiles })
